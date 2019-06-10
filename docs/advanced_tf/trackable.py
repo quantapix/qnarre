a1c772da9aeb66c3f23dbd2ef5e5b1049a33cc5d
quantapix <quantapix@gmail.com>
2019-06-10 12:52:03 -0400

    qpx

--- docs/advanced_tf/trackable.py
+++ docs/advanced_tf/trackable.py
@@ -16,6 +16,7 @@
 
 import tensorflow as tf
 
+# from tensorflow.python.module import module
 from tensorflow.python.training.tracking import base
 from tensorflow.python.training.tracking import tracking
 
@@ -24,7 +25,7 @@ def trackable(tr1, v):
     c = tf.train.Checkpoint(tr1=tr1)
     m = tf.train.CheckpointManager(c, '/tmp/trackable', max_to_keep=2)
     p = m.latest_checkpoint
-    c.restore(p)
+    c.restore(p).expect_partial()
     if p:
         print(f'restored from: {p}')
         print(f'others are: {m.checkpoints}')
@@ -85,6 +86,75 @@ def containers(tr3):
     print(f'list containers: {vs}')
 
 
+def sharing(tr3):
+    c = tf.train.Checkpoint(tr3=tr3)
+    m = tf.train.CheckpointManager(c, '/tmp/trackable', max_to_keep=2)
+    c.restore(m.latest_checkpoint).assert_consumed()
+    v1 = tr3.br_list[0].v
+    v2 = tr3.br_list[1].v
+    vd1 = tr3.br_dict['br1'].v
+    vd2 = tr3.br_dict['br2'].v
+    vd3 = tr3.br_dict['br3'].v
+    print(f'all fives: {v1.numpy()}, {v2.numpy()}, {vd3.numpy()}')
+    print(f'shared too: {vd1.numpy()}, {vd2.numpy()}')
+    v1.assign_add(5)
+    v2.assign_add(5)
+    vd3.assign_add(5)
+    m.save()
+    vs = tf.train.list_variables(m.latest_checkpoint)
+    print(f'shared not repeated: {vs}')
+    v1.assign_add(-10)
+    v2.assign_add(-10)
+    vd3.assign_add(-10)
+    print(f'all zeros: {v1.numpy()}, {v2.numpy()}, {vd3.numpy()}')
+    print(f'shared too: {vd1.numpy()}, {vd2.numpy()}')
+    c2 = tf.train.Checkpoint(tr3=tr3)
+    m = tf.train.CheckpointManager(c2, '/tmp/trackable', max_to_keep=2)
+    c2.restore(m.latest_checkpoint).assert_consumed()
+    print(f'all tens: {v1.numpy()}, {v2.numpy()}, {vd3.numpy()}')
+    print(f'shared too: {vd1.numpy()}, {vd2.numpy()}')
+
+
+class Module(tf.Module):
+    sub = None
+
+    def __init__(self, name=None):
+        super().__init__(name=name)
+        self.v = tf.Variable(0, name='m_v')
+
+    @tf.function
+    def __call__(self):
+        y = tf.math.add(self.v, tf.constant(1))
+        if self.sub is not None:
+            y = tf.math.add(y, self.sub())
+        self.v.assign(y)
+        return y
+
+    def print_all(self):
+        print(f'{self.name}: {self.v.numpy()}')
+        if self.sub:
+            self.sub.print_all()
+
+
+def modules(mod1):
+    # vs = [v.name for v in mod1.variables]
+    # ms = [m.name for m in mod1.submodules]
+    # print(f'mod variables: {vs}, submodules: {ms}')
+    c = tf.train.Checkpoint(mod1=mod1)
+    m = tf.train.CheckpointManager(c, '/tmp/trackable', max_to_keep=2)
+    mod1()
+    mod1.print_all()
+    m.save()
+    mod1()
+    mod1.print_all()
+    p = m.latest_checkpoint
+    vs = tf.train.list_variables(p)
+    print(f'list containers: {vs}')
+    c.restore(p)
+    print('after restore:')
+    mod1.print_all()
+
+
 def main(_):
     tr1 = base.Trackable()
     v = tf.Variable(1)
@@ -104,14 +174,23 @@ def main(_):
 
     tr3 = tracking.AutoTrackable()
     br1 = tracking.AutoTrackable()
-    br1.v = tf.Variable(0)
+    br1.v = tf.Variable(5)
     br2 = tracking.AutoTrackable()
-    br2.v = tf.Variable(0)
+    br2.v = tf.Variable(5)
     tr3.br_list = [br1, br2]
     br3 = tracking.AutoTrackable()
-    br3.v = tf.Variable(0)
+    br3.v = tf.Variable(5)
     tr3.br_dict = {'br3': br3}
     containers(tr3)
+    tr3.br_dict = {'br1': br1, 'br2': br2, 'br3': br3}
+    sharing(tr3)
+
+    mod1 = Module('m1')
+    mod1.sub = Module('m2')
+    mod1.sub.sub = Module('m3')
+    mod1.sub.sub.sub = Module('m4')
+    mod1.sub.sub.sub.sub = Module('m5')
+    modules(mod1)
     """
     opt = tf.keras.optimizers.Adam(0.1)
     ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=opt, net=net)
