195b208618c3eb3a901144fe512330e77043ecf4
quantapix <quantapix@gmail.com>
2019-06-20 19:17:51 -0400

    qpx

--- docs/advanced_tf/dataset.py
+++ docs/advanced_tf/dataset.py
@@ -17,9 +17,11 @@
 # export TF_XLA_FLAGS=--tf_xla_cpu_global_jit
 
 import numpy as np
+import pathlib as pth
 import tensorflow as tf
 
 td = tf.data
+tt = tf.train
 # ks = tf.keras
 # kl = ks.layers
 
@@ -27,7 +29,7 @@ td = tf.data
 # a pipeline starts with a "source" and chains "transformations" to it
 
 # example - fascinating https://arxiv.org/pdf/1812.02825.pdf
-# num_items of "x=-12,y=24:y+x:12" w/ "defs", "op" and "res"
+# num_samples of "x=-12,y=24:y+x:12" w/ "defs", "op" and "res"
 # names: x, y, ops: +, -, *, values: [-max_val, max_val]
 
 vocab = ('x', 'y')
@@ -38,7 +40,9 @@ vocab += ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 
 params = dict(
     max_val=10,
-    num_items=4,
+    num_samples=4,
+    num_shards=3,
+    size_batch=2,
 )
 
 
@@ -48,8 +52,8 @@ class Params:
             setattr(self, k, v)
 
 
-def native_source(ps):
-    m, n = ps.max_val, ps.num_items
+def native(ps):
+    m, n = ps.max_val, ps.num_samples
     # x, y values in defs
     vals = np.random.randint(low=-m, high=m + 1, size=(2, n))
     # (x, y) order if 1 in defs [0] and op [1], respectively
@@ -79,31 +83,34 @@ def native_source(ps):
 # can be consumed as iterables or as aggregatables (reduce)
 
 
-def dset_gen_src(ps):
+def gen_src(ps):
     # from_generator
     ds = td.Dataset.from_generator(
-        lambda: native_source(ps),
+        lambda: native(ps),
         tf.string,
         tf.TensorShape([]),
     )
     return ds
 
 
-def dset_src(ps):
+def source(ps):
     # range, from_tensor_slices, from_tensors
-    ds = [s for s in native_source(ps)]
+    # also TextLineDataset
+    ds = [s for s in native(ps)]
     ds = td.Dataset.from_tensor_slices(np.array(ds))
     return ds
 
 
-def dset_filter(x):
+# inside a tf.function or eagerly
+@tf.function
+def filterer(x):
     r = tf.strings.length(x) < 15
     tf.print(tf.strings.format('filtering {}... ', x) + ('in' if r else 'out'))
     return r
 
 
 @tf.function
-def dset_features(x):
+def splitter(x):
     fs = tf.strings.split(x, ':')
     return {'defs': fs[0], 'op': fs[1], 'res': fs[2]}
 
@@ -112,7 +119,7 @@ tokens = {k: v for v, k in enumerate(vocab, start=5)}
 
 
 @tf.function
-def dset_tokenize(d):
+def tokenizer(d):
     return {
         k: tf.numpy_function(
             lambda x: tf.constant([tokens[chr(c)] for c in x]),
@@ -124,36 +131,95 @@ def dset_tokenize(d):
 
 
 # potentially large
-# inside a tf.function or eagerly
 # transparent performance is key
+
+
+def shards(ps):
+    for _ in range(ps.num_shards):
+        yield source(ps).map(splitter).map(tokenizer)
+
+
+def records(dset):
+    for s in dset:
+        fs = tt.Features(
+            feature={
+                'defs': tt.Feature(int64_list=tt.Int64List(value=s['defs'])),
+                'op': tt.Feature(int64_list=tt.Int64List(value=s['op'])),
+                'res': tt.Feature(int64_list=tt.Int64List(value=s['res'])),
+            })
+        yield tt.Example(features=fs).SerializeToString()
+
+
+def dump(ps):
+    root = pth.Path('/tmp/qnarre/dataset')
+    root.mkdir(parents=True, exist_ok=True)
+    for i, ds in enumerate(shards(ps)):
+        p = str(root / f'shard_{i}.tfrecords')
+        with tf.io.TFRecordWriter(p) as w:
+            for r in records(ds):
+                w.write(r)
+        yield p
+
+
+features = {
+    'defs': tf.io.VarLenFeature(tf.int64),
+    'op': tf.io.VarLenFeature(tf.int64),
+    'res': tf.io.VarLenFeature(tf.int64),
+}
+
+
+@tf.function
+def load(paths):
+    ds = td.TFRecordDataset(paths)
+    ds = ds.map(lambda x: tf.io.parse_single_example(x, features))
+    return ds.map(
+        lambda d: {
+            'defs': tf.sparse.to_dense(d['defs']),
+            'op': tf.sparse.to_dense(d['op']),
+            'res': tf.sparse.to_dense(d['res']),
+        })
+
+
+@tf.function
+def batch_load(ps, paths):
+    ds = td.TFRecordDataset(paths).batch(ps.size_batch)
+    ds = ds.map(lambda x: tf.io.parse_example(x, features))
+    return ds.map(
+        lambda d: {
+            'defs': tf.sparse.to_dense(d['defs']),
+            'op': tf.sparse.to_dense(d['op']),
+            'res': tf.sparse.to_dense(d['res']),
+        })
 
 
 def main(_):
     ps = Params(**params)
-    for s in native_source(ps):
+    for s in native(ps):
         print(s)
     # cache, concatenate, enumerate, reduce, repeat, shuffle, skip, take, zip
     print('Ops on datasets')
-    dg = dset_gen_src(ps)
+    dg = gen_src(ps)
     for s in dg.take(2):
         print(s)
-    ds = dset_src(ps)
+    ds = source(ps)
     for i, s in ds.take(2).concatenate(dg).enumerate():
         print(i, s)
     # filter
     print('Filter dataset elements')
-    for i, s in enumerate(ds.filter(dset_filter)):
-        print(s)
+    for i, s in enumerate(ds.filter(filterer)):
+        print(i, s)
     # map
-    for s in ds.map(dset_features).take(1):
+    for s in ds.map(splitter).take(1):
         print(s)
-    for s in ds.map(dset_features).map(dset_tokenize).take(1):
+    for s in ds.map(splitter).map(tokenizer).take(1):
         print(s)
-    # apply, flat_map, interleave
-
+    fs = [f for f in dump(ps)]
+    for i, s in enumerate(load(fs)):
+        print(i, s)
     # batch, padded_batch
-
-    # prefetch
+    for i, s in enumerate(batch_load(ps, fs)):
+        print(i, s)
+    # apply, flat_map, interleave, prefetch
 
 
 if __name__ == '__main__':
