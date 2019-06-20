5e7adaea840f09e1ad0f70a5a6359d6fed340f53
quantapix <quantapix@gmail.com>
2019-06-20 12:57:19 -0400

    qpx

--- docs/advanced_tf/dataset.py
+++ docs/advanced_tf/dataset.py
@@ -12,37 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =============================================================================
+# !pip install -U tf-nightly-2.0-preview
 # !pip install tensorflow==2.0.0-beta0
+# export TF_XLA_FLAGS=--tf_xla_cpu_global_jit
 
 import numpy as np
 import tensorflow as tf
 
 td = tf.data
-ks = tf.keras
-kl = ks.layers
+# ks = tf.keras
+# kl = ks.layers
 
 # complex input pipelines from simple, reusable pieces
 # a pipeline starts with a "source" and chains "transformations" to it
 
-# tf.data.Dataset - abstraction for potentially large sequence of elements
-# each element is one or more Tensors
-# can be consumed as iterables or as aggregatables (reduce)
-# [inside a tf.function or eagerly]
+# example - fascinating https://arxiv.org/pdf/1812.02825.pdf
+# num_items of "x=-12,y=24:y+x:12" w/ "defs", "op" and "res"
+# names: x, y, ops: +, -, *, values: [-max_val, max_val]
+# vocab: ['x', 'y', '+', '-', '*', '=', ',', ':']
+# also: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-# apply, batch, cache,
-
-# concatenate, enumerate, filter, flat_map, interleave
-
-# from_generator, from_tensor_slices, from_tensors
-
+# pipeline is parametric
 
 params = dict(
-    dim_hidden=1000,
-    dim_input=100,
-    loss=ks.losses.MeanAbsoluteError,
-    metrics=ks.metrics.MeanAbsoluteError,
-    num_layers=10,
-    optimizer=ks.optimizers.SGD,
+    max_val=10,
+    num_items=4,
 )
 
 
@@ -52,9 +46,87 @@ class Params:
             setattr(self, k, v)
 
 
+def native_source(ps):
+    m, n = ps.max_val, ps.num_items
+    # x, y values in defs
+    vals = np.random.randint(low=-m, high=m + 1, size=(2, n))
+    # (x, y) order if 1 in defs [0] and op [1], respectively
+    ords = np.random.randint(2, size=(2, n))
+    # index of ['+', '-', '*']
+    ops = np.array(['+', '-', '*'])
+    ops.reshape((1, 3))
+    ops = ops[np.random.randint(3, size=n)]
+    for i in range(n):
+        x, y = vals[:, i]
+        res = f'x={x},y={y}:' if ords[0, i] else f'y={y},x={x}:'
+        o = ops[i]
+        res += (f'x{o}y:' if ords[1, i] else f'y{o}x:')
+        if o == '+':
+            res += f'{x + y}'
+        elif o == '*':
+            res += f'{x * y}'
+        else:
+            assert o == '-'
+            res += (f'{x - y}' if ords[1, i] else f'{y - x}')
+        yield res
+
+
+# tf.data.Dataset - abstraction for a sequence of elements
+# each element is one or more Tensors
+
+# can be consumed as iterables or as aggregatables (reduce)
+
+def dset_gen_src(ps):
+    # from_generator
+    ds = td.Dataset.from_generator(
+        lambda: native_source(ps),
+        tf.string,
+        tf.TensorShape([]),
+    )
+    return ds
+
+
+def dset_src(ps):
+    # range, from_tensor_slices, from_tensors
+    ds = [s for s in native_source(ps)]
+    ds = td.Dataset.from_tensor_slices(np.array(ds))
+    return ds
+
+
+def dset_filter(x):
+    r = tf.strings.length(x) < 15
+    tf.print(tf.strings.format('filtering {}... ', x) + ('in' if r else 'out'))
+    return r
+
+
+# potentially large
+# inside a tf.function or eagerly
+# transparent performance is key
+
+
 def main(_):
     ps = Params(**params)
-    d = np.ones((100, ps.dim_input))
+    for s in native_source(ps):
+        print(s)
+    # cache, concatenate, enumerate, reduce, repeat, shuffle, skip, take, zip
+    print('Ops on datasets')
+    ds1 = dset_gen_src(ps)
+    ds2 = dset_src(ps)
+    for s in ds1.take(2):
+        print(s)
+    for i, s in ds2.take(2).concatenate(ds1).enumerate():
+        print(i, s)
+    # filter
+    print('Filter dataset elements')
+    ds = dset_src(ps)
+    for i, s in enumerate(ds.filter(dset_filter)):
+        print(s)
+
+    # apply, flat_map, interleave, map
+
+    # batch, padded_batch
+
+    # prefetch
 
 
 if __name__ == '__main__':
