5e01e5534749a14c2fd334bfe95f2074fcf3fb6e
quantapix <quantapix@gmail.com>
2019-06-29 17:42:34 -0400

    qpx

--- docs/advanced_tf/metrics.py
+++ docs/advanced_tf/metrics.py
@@ -25,56 +25,31 @@ ks = tf.keras
 kl = ks.layers
 
 
-@tf.function
-def adapter(d, target):
-    enc, dec, tgt = d['enc'], d['dec'], d['tgt']
-    target.assign(tgt.to_tensor())
-    return (
-        (
-            enc.flat_values,
-            enc.row_splits,
-            dec.flat_values,
-            dec.row_splits,
-            tgt.flat_values,
-            tgt.row_splits,
-        ),
-        tgt.to_tensor()
-    )
-
-
-def dset_for(ps, target):
-    ds = tf.data.TFRecordDataset(list(qd.files(ps)))
-    ds = ds.take(1000).batch(ps.dim_batch)
-    fs = {
-        'defs': tf.io.VarLenFeature(tf.int64),
-        'op': tf.io.VarLenFeature(tf.int64),
-        'res': tf.io.VarLenFeature(tf.int64),
-    }
-    ds = ds.map(lambda x: tf.io.parse_example(x, fs)).map(qd.caster)
-    return ds.map(qc.formatter).map(lambda x: adapter(x, target))
-
-
 class Loss(ks.losses.Loss):
     @staticmethod
-    def xent(y_true, y_pred):
-        kw = dict(labels=y_true, logits=y_pred)
-        return tf.nn.sparse_softmax_cross_entropy_with_logits(**kw)
+    def xent(tgt, out):
+        tgt = tf.reshape(tf.cast(tgt, tf.int64), [-1])
+        s = tf.shape(out)
+        out = tf.reshape(out, [-1, s[-1]])
+        y = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tgt,
+                                                           logits=out)
+        return tf.reshape(y, s[:-1])
 
     def __init__(self):
-        super().__init__(name='loss')
+        super().__init__(name='qloss')
 
-    def call(self, y_true, y_pred):
-        return self.xent(y_true, y_pred)
+    def call(self, tgt, out):
+        return self.xent(tgt, out)
 
 
 class Metric(ks.metrics.Metric):
     def __init__(self):
-        super().__init__(name='metric', dtype=tf.float32)
+        super().__init__(name='qmetric', dtype=tf.float32)
         self.total = self.add_weight('total', initializer='zeros')
         self.count = self.add_weight('count', initializer='zeros')
 
-    def update_state(self, y_true, y_pred, sample_weight=None):
-        vs = Loss.xent(y_true, y_pred)
+    def update_state(self, tgt, out, sample_weight=None):
+        vs = Loss.xent(tgt, out)
         self.total.assign_add(tf.math.reduce_sum(vs))
         return self.count.assign_add(tf.cast(tf.size(vs), dtype=tf.float32))
 
@@ -91,20 +66,17 @@ params.update(
 
 def main(_):
     ps = qd.Params(**params)
-    with tf.Graph().as_default():
-        target = tf.Variable(0, shape=tf.TensorShape(None))
-        ds = dset_for(ps, target)
-        m = qc.model_for(ps)
-        m.compile(
-            optimizer=ps.optimizer,
-            loss={'debed': ps.loss},
-            metrics={'debed': [ps.metric]},
-            target_tensors={'debed': target},
-        )
-        ld = datetime.now().strftime('%Y%m%d-%H%M%S')
-        ld = f'/tmp/q/logs/{ld}'
-        cs = [ks.callbacks.TensorBoard(log_dir=ld, histogram_freq=1)]
-        m.fit(ds, callbacks=cs, epochs=ps.num_epochs)
+    ds = qc.dset_for(ps)
+    m = qc.model_for(ps)
+    m.compile(
+        optimizer=ps.optimizer,
+        loss={'debed': ps.loss},
+        metrics={'debed': [ps.metric]},
+    )
+    ld = datetime.now().strftime('%Y%m%d-%H%M%S')
+    ld = f'/tmp/q/logs/{ld}'
+    cs = [ks.callbacks.TensorBoard(log_dir=ld, histogram_freq=1)]
+    m.fit(ds, callbacks=cs, epochs=ps.num_epochs)
 
 
 if __name__ == '__main__':
