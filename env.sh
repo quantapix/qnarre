a8dadb3c87ac32e99f2a905a4af6695623b06339
quantapix <quantapix@gmail.com>
2023-04-06 13:15:56 -0400

    qpx

--- env.sh
+++ env.sh
@@ -6,7 +6,7 @@
 # jupyter notebook --no-browser --port=8889
 # ssh -N -f -L localhost:8888:localhost:8889 remote_user@remote_host
 
-# jupyter nbconvert --to script 
+# jupyter nbconvert --to script
 
 # export PATH=/home/qpix/clone/qnarre_new/.nvold/cuda/bin:$PATH
 # export LD_LIBRARY_PATH=/home/qpix/clone/qnarre_new/.nvold/cuda/lib64:$LD_LIBRARY_PATH
@@ -14,28 +14,33 @@
 
 nvenv() {
     mkdir .nvenv
-    (cd nvidia
-     sh ./cuda_* --extract="$OLDPWD"/.nvenv --override)
+    (
+        cd nvidia
+        sh ./cuda_* --extract="$OLDPWD"/.nvenv --override
+    )
     mv .nvenv/cuda-toolkit .nvenv/cuda
     mv .nvenv/cuda-samples .nvenv/cuda/samples
-    (cd nvidia
-     tar xf cudnn_* -C "$OLDPWD"/.nvenv
-     tar xf nccl_* -C "$OLDPWD"/.nvenv
-     tar xf tensorrt_* -C "$OLDPWD"/.nvenv)
+    (
+        cd nvidia
+        tar xf cudnn_* -C "$OLDPWD"/.nvenv
+        tar xf nccl_* -C "$OLDPWD"/.nvenv
+        tar xf tensorrt_* -C "$OLDPWD"/.nvenv
+    )
     mv .nvenv/nccl_* .nvenv/nccl
     mv .nvenv/TensorRT-* .nvenv/tensorrt
     sed -i "/.*unsupported GNU version.*/d" .nvenv/cuda/targets/x86_64-linux/include/crt/host_config.h
 }
 
 keras() {
-    (cd keras/upstream
-     ../../.env/bin/pip install -e .
+    (
+        cd keras/upstream
+        ../../.env/bin/pip install -e .
     )
 }
 
 spacy() {
     if $1; then
-        .env/bin/pip install spacy[lookups,transformers,cuda115]
+        .env/bin/pip install spacy[lookups,transformers,cuda118]
     else
         .env/bin/pip install spacy[lookups,transformers]
     fi
@@ -51,26 +56,30 @@ tflow() {
 
 ptorch() {
     if $1; then
-        .env/bin/pip install -U cupy-cuda115
-        .env/bin/pip install --pre torch torchtext torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu115/torch_nightly.html
+        .env/bin/pip install -U cupy-cuda118
+        .env/bin/pip install --pre torch torchtext torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu118
     else
-        .env/bin/pip install --pre torch torchtext torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
+        .env/bin/pip install --pre torch torchtext torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
     fi
     .env/bin/pip install -U captum tensorboard torch-tb-profiler
 }
 
 hface() {
-    (cd lib/tokenizers/bindings/python
+    (
+        cd lib/tokenizers/bindings/python
         ../../../../.env/bin/pip install setuptools_rust
         ../../../../.env/bin/python setup.py install
     )
-    (cd lib/accelerate
+    (
+        cd lib/accelerate
         ../../.env/bin/pip install -e .
     )
-    (cd lib/datasets
+    (
+        cd lib/datasets
         ../../.env/bin/pip install -e .
     )
-    (cd lib/transformers
+    (
+        cd lib/transformers
         ../../.env/bin/pip install -e .
     )
 }
@@ -85,20 +94,23 @@ main() {
     local CLEAN=false
 
     while getopts "cgh" opt; do
-	      case $opt in
-	          c) CLEAN=true;;
-            g) GPU=true;;
-	          *) show_usage; return 1;;
-	      esac
+        case $opt in
+        c) CLEAN=true ;;
+        g) GPU=true ;;
+        *)
+            show_usage
+            return 1
+            ;;
+        esac
     done
-    shift $((OPTIND-1))
+    shift $((OPTIND - 1))
 
     if "$CLEAN"; then
-	      rm -rf .env
+        rm -rf .env
     fi
 
     if [ ! -e .env ]; then
-	      python3.10 -m venv .env
+        python3.11 -m venv .env
     fi
 
     .env/bin/pip install -U pip wheel setuptools pytest black
@@ -110,7 +122,7 @@ main() {
     # spacy "$GPU"
     .env/bin/pip install -U tokenizers accelerate datasets transformers
     # hface
-    # .env/bin/pip install -U regex cytoolz joblib scikit-learn  
+    # .env/bin/pip install -U regex cytoolz joblib scikit-learn
     # .env/bin/pip install -U pycodestyle pylint pyyaml
     # .env/bin/pip install -U flake8 autopep8 jedi yapf
     # .env/bin/pip install -U gin-config  sympy gym pypng spacy-nightly
