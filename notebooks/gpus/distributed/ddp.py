c84925183812d94a75a110e627f52e05fe6bcd48
quantapix <quantapix@gmail.com>
2023-04-20 15:15:09 +0000

    qpx

--- notebooks/gpus/distributed/ddp.py
+++ notebooks/gpus/distributed/ddp.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import tempfile
 
 import torch
 import torch.distributed as dist
@@ -32,9 +33,65 @@ def run(world_size, rank):
     ddp = DDP(model, ids)
     loss = nn.MSELoss()
     optimizer = optim.SGD(ddp.parameters(), lr=0.001)
+
     optimizer.zero_grad()
     ys = ddp(torch.randn(20, 10))
     labels = torch.randn(20, 5).to(ids[0])
+    loss(ys, labels).backward()
+    optimizer.step()
+
+
+def run_checkpoint(world_size, rank):
+    model = Model().to(rank)
+    ddp = DDP(model, device_ids=[rank])
+    loss = nn.MSELoss()
+    optimizer = optim.SGD(ddp.parameters(), lr=0.001)
+
+    CHECKPOINT_PATH = tempfile.gettempdir() + "/model.checkpoint"
+    if rank == 0:
+        torch.save(ddp.state_dict(), CHECKPOINT_PATH)
+    dist.barrier()
+    map_location = {"cuda:%d" % 0: "cuda:%d" % rank}
+    ddp.load_state_dict(torch.load(CHECKPOINT_PATH, map_location=map_location))
+
+    optimizer.zero_grad()
+    ys = ddp(torch.randn(20, 10))
+    labels = torch.randn(20, 5).to(rank)
+    loss(ys, labels).backward()
+    optimizer.step()
+
+    dist.barrier()
+    if rank == 0:
+        os.remove(CHECKPOINT_PATH)
+
+
+class MpModel(nn.Module):
+    def __init__(self, dev0, dev1):
+        super(MpModel, self).__init__()
+        self.dev0 = dev0
+        self.dev1 = dev1
+        self.net1 = torch.nn.Linear(10, 10).to(dev0)
+        self.relu = torch.nn.ReLU()
+        self.net2 = torch.nn.Linear(10, 5).to(dev1)
+
+    def forward(self, x):
+        x = x.to(self.dev0)
+        x = self.relu(self.net1(x))
+        x = x.to(self.dev1)
+        return self.net2(x)
+
+
+def run_mp(world_size, rank):
+    dev0 = rank * 2
+    dev1 = rank * 2 + 1
+    model = MpModel(dev0, dev1)
+    ddp = DDP(model)
+    loss = nn.MSELoss()
+    optimizer = optim.SGD(ddp.parameters(), lr=0.001)
+
+    optimizer.zero_grad()
+    ys = ddp(torch.randn(20, 10))
+    labels = torch.randn(20, 5).to(dev1)
     loss(ys, labels).backward()
     optimizer.step()
 
