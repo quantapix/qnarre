fa0f5dfe3a43a4e2c9cab7c39bc13d279b0ecf82
quantapix <quantapix@gmail.com>
2023-04-24 18:12:01 +0000

    qpx

--- qnarre/core/output.py
+++ qnarre/core/output.py
@@ -1,238 +1,201 @@
-from collections import OrderedDict
 from dataclasses import fields, dataclass
-from transformers.file_utils import is_tensor
 
 
-class Output(OrderedDict):
-    def __post_init__(self):
-        fs = fields(self)
-        assert len(fs)
-        assert all(f.default is None for f in fs[1:])
-        first = getattr(self, fs[0].name)
-        others_none = all(getattr(self, f.name) is None for f in fs[1:])
-        if others_none and not is_tensor(first):
-            if isinstance(first, dict):
-                xs = first.items()
-                first_iter = True
+class Output(dict):
+    def __init__(self, *xs, **kw):
+        n = len(xs)
+        if n > 0:
+            x0 = xs[0]
+            if isinstance(x0, dict):
+                x0.update(kw)
+                kw = x0
+                n = 0
             else:
                 try:
-                    xs = iter(first)
-                    first_iter = True
+                    for x in iter(x0):
+                        if (
+                            not isinstance(x, (list, tuple))
+                            or not len(x) == 2
+                            or not isinstance(x[0], str)
+                        ):
+                            break
+                        if x[1] is not None:
+                            kw.update(tuple(x))
+                        n = 0
                 except TypeError:
-                    first_iter = False
-            if first_iter:
-                for x in xs:
-                    if (
-                        not isinstance(x, (list, tuple))
-                        or not len(x) == 2
-                        or not isinstance(x[0], str)
-                    ):
-                        break
-                    setattr(self, x[0], x[1])
-                    if x[1] is not None:
-                        self[x[0]] = x[1]
-            elif first is not None:
-                self[fs[0].name] = first
-        else:
-            for f in fs:
-                v = getattr(self, f.name)
-                if v is not None:
-                    self[f.name] = v
-
-    def __delitem__(self, *args, **kw):
-        raise Exception()
-
-    def setdefault(self, *args, **kw):
-        raise Exception()
-
-    def pop(self, *args, **kw):
-        raise Exception()
-
-    def update(self, *args, **kw):
-        raise Exception()
-
-    def __getitem__(self, k):
-        if isinstance(k, str):
-            return {k: v for (k, v) in self.items()}[k]
-        else:
-            return self.to_tuple()[k]
-
-    def __setattr__(self, k, v):
-        if k in self.keys() and v is not None:
-            super().__setitem__(k, v)
-        super().__setattr__(k, v)
-
-    def __setitem__(self, k, v):
-        super().__setitem__(k, v)
-        super().__setattr__(k, v)
-
-    def to_tuple(self):
-        return tuple(self[k] for k in self.keys())
+                    pass
+        for i, f in enumerate(fields(self)):
+            v = xs[i] if i < n else None
+            print(f.name, ": ", v)
+            if v is None:
+                v = kw.get(f.name, f.default)
+            setattr(self, f.name, v)
+            self[f.name] = v
 
 
-@dataclass
+@dataclass(init=False)
 class Base(Output):
-    y: tuple = None
-    attns: tuple = None
-    hiddens: tuple = None
-    globals: tuple = None
+    y: tuple | None = None
+    attns: tuple | None = None
+    hiddens: tuple | None = None
+    globals: tuple | None = None
 
 
 @dataclass
 class WithCaches(Output):
-    y: tuple = None
-    attns: tuple = None
-    caches: tuple = None
-    hiddens: tuple = None
+    y: tuple | None = None
+    attns: tuple | None = None
+    caches: tuple | None = None
+    hiddens: tuple | None = None
 
 
 @dataclass
 class WithCrosses(Output):
-    y: tuple = None
-    attns: tuple = None
-    crosses: tuple = None
-    hiddens: tuple = None
+    y: tuple | None = None
+    attns: tuple | None = None
+    crosses: tuple | None = None
+    hiddens: tuple | None = None
 
 
 @dataclass
 class WithLoss(Output):
-    logits = None
-    attns: tuple = None
-    hiddens: tuple = None
-    globals: tuple = None
-    loss = None
+    logits: tuple | None = None
+    attns: tuple | None = None
+    hiddens: tuple | None = None
+    globals: tuple | None = None
+    loss: tuple | None = None
 
 
 @dataclass
 class WithMems(Output):
-    y: tuple = None
-    attns: tuple = None
-    hiddens: tuple = None
-    mems: tuple = None
+    y: tuple | None = None
+    attns: tuple | None = None
+    hiddens: tuple | None = None
+    mems: tuple | None = None
 
 
 @dataclass
 class WithPools(Output):
-    y: tuple = None
-    attns: tuple = None
-    hiddens: tuple = None
-    globals: tuple = None
-    pools: tuple = None
+    y: tuple | None = None
+    attns: tuple | None = None
+    hiddens: tuple | None = None
+    globals: tuple | None = None
+    pools: tuple | None = None
 
 
 @dataclass
 class CachesCrosses(Output):
-    y: tuple = None
-    attns: tuple = None
-    caches: tuple = None
-    crosses: tuple = None
-    hiddens: tuple = None
+    y: tuple | None = None
+    attns: tuple | None = None
+    caches: tuple | None = None
+    crosses: tuple | None = None
+    hiddens: tuple | None = None
 
 
 @dataclass
 class PoolsCrosses(Output):
-    y: tuple = None
-    attns: tuple = None
-    caches: tuple = None
-    crosses: tuple = None
-    hiddens: tuple = None
-    pools: tuple = None
+    y: tuple | None = None
+    attns: tuple | None = None
+    caches: tuple | None = None
+    crosses: tuple | None = None
+    hiddens: tuple | None = None
+    pools: tuple | None = None
 
 
 @dataclass
 class Seq2Seq(Output):
-    y: tuple = None
-    attns: tuple = None
-    caches: tuple = None
-    crosses: tuple = None
-    hiddens: tuple = None
-    enc_y: tuple = None
-    enc_attns: tuple = None
-    enc_hiddens: tuple = None
-    enc_globals: tuple = None
+    y: tuple | None = None
+    attns: tuple | None = None
+    caches: tuple | None = None
+    crosses: tuple | None = None
+    hiddens: tuple | None = None
+    enc_y: tuple | None = None
+    enc_attns: tuple | None = None
+    enc_hiddens: tuple | None = None
+    enc_globals: tuple | None = None
 
 
 @dataclass
 class LossCaches(Output):
-    logits = None
-    attns: tuple = None
-    caches: tuple = None
-    hiddens: tuple = None
-    loss = None
+    logits: tuple | None = None
+    attns: tuple | None = None
+    caches: tuple | None = None
+    hiddens: tuple | None = None
+    loss: tuple | None = None
 
 
 @dataclass
 class LossCrosses(Output):
-    logits = None
-    attns: tuple = None
-    caches: tuple = None
-    crosses: tuple = None
-    hiddens: tuple = None
-    loss = None
+    logits: tuple | None = None
+    attns: tuple | None = None
+    caches: tuple | None = None
+    crosses: tuple | None = None
+    hiddens: tuple | None = None
+    loss: tuple | None = None
 
 
 @dataclass
 class LossMems(Output):
-    logits: tuple = None
-    attns: tuple = None
-    hiddens: tuple = None
-    mems: tuple = None
-    loss: tuple = None
+    logits: tuple | None = None
+    attns: tuple | None = None
+    hiddens: tuple | None = None
+    mems: tuple | None = None
+    loss: tuple | None = None
 
 
 @dataclass
 class LossQA(Output):
-    logits_beg = None
-    logits_end = None
-    attns: tuple = None
-    hiddens: tuple = None
-    globals: tuple = None
-    loss = None
+    logits_beg: tuple | None = None
+    logits_end: tuple | None = None
+    attns: tuple | None = None
+    hiddens: tuple | None = None
+    globals: tuple | None = None
+    loss: tuple | None = None
 
 
 @dataclass
 class LossQAPools(Output):
-    logits_beg: tuple = None
-    logits_end: tuple = None
-    attns: tuple = None
-    hiddens: tuple = None
-    pools: tuple = None
-    loss: tuple = None
+    logits_beg: tuple | None = None
+    logits_end: tuple | None = None
+    attns: tuple | None = None
+    hiddens: tuple | None = None
+    pools: tuple | None = None
+    loss: tuple | None = None
 
 
 @dataclass
 class LossSeq(Output):
-    logits: tuple = None
-    orders: tuple = None
-    attns: tuple = None
-    hiddens: tuple = None
-    loss: tuple = None
+    logits: tuple | None = None
+    orders: tuple | None = None
+    attns: tuple | None = None
+    hiddens: tuple | None = None
+    loss: tuple | None = None
 
 
 @dataclass
 class LossSeq2Seq(Output):
-    logits = None
-    attns: tuple = None
-    caches: tuple = None
-    crosses: tuple = None
-    hiddens: tuple = None
-    enc_y: tuple = None
-    enc_attns: tuple = None
-    enc_hiddens: tuple = None
-    enc_globals: tuple = None
-    loss = None
+    logits: tuple | None = None
+    attns: tuple | None = None
+    caches: tuple | None = None
+    crosses: tuple | None = None
+    hiddens: tuple | None = None
+    enc_y: tuple | None = None
+    enc_attns: tuple | None = None
+    enc_hiddens: tuple | None = None
+    enc_globals: tuple | None = None
+    loss: tuple | None = None
 
 
 @dataclass
 class LossSeq2SeqQA(Output):
-    logits_beg = None
-    logits_end = None
-    caches: tuple = None
-    crosses: tuple = None
-    attns: tuple = None
-    hiddens: tuple = None
-    enc_y: tuple = None
-    enc_attns: tuple = None
-    enc_hiddens: tuple = None
-    enc_globals: tuple = None
-    loss = None
+    logits_beg: tuple | None = None
+    logits_end: tuple | None = None
+    caches: tuple | None = None
+    crosses: tuple | None = None
+    attns: tuple | None = None
+    hiddens: tuple | None = None
+    enc_y: tuple | None = None
+    enc_attns: tuple | None = None
+    enc_hiddens: tuple | None = None
+    enc_globals: tuple | None = None
+    loss: tuple | None = None
