11ae9ae608603c25ef9bc5a17838be1be593b212
quantapix <quantapix@gmail.com>
2023-05-09 01:25:01 +0000

    qpx

--- qnarre/try/add.py
+++ qnarre/try/add.py
@@ -8,12 +8,12 @@ import triton.language as tl
 @triton.jit
 def add_kernel(x1_ptr, x2_ptr, y_ptr, n, BLOCK: tl.constexpr):
     pid = tl.program_id(axis=0)
-    offsets = pid * BLOCK + tl.arange(0, BLOCK)
-    mask = offsets < n
-    x1 = tl.load(x1_ptr + offsets, mask=mask)
-    x2 = tl.load(x2_ptr + offsets, mask=mask)
+    offs = pid * BLOCK + tl.arange(0, BLOCK)
+    mask = offs < n
+    x1 = tl.load(x1_ptr + offs, mask=mask)
+    x2 = tl.load(x2_ptr + offs, mask=mask)
     y = x1 + x2
-    tl.store(y_ptr + offsets, y, mask=mask)
+    tl.store(y_ptr + offs, y, mask=mask)
 
 
 # %%
@@ -23,7 +23,7 @@ def add(x1: torch.Tensor, x2: torch.Tensor):
     y = torch.empty_like(x1)
     assert x1.is_cuda and x2.is_cuda and y.is_cuda
     n = y.numel()
-    grid = lambda meta: (triton.cdiv(n, meta["BLOCK"]),)
+    grid = lambda x: (triton.cdiv(n, x["BLOCK"]),)
     add_kernel[grid](x1, x2, y, n, BLOCK=1024)
     return y
 
@@ -36,8 +36,8 @@ x1 = torch.rand(size, device="cuda")
 x2 = torch.rand(size, device="cuda")
 y_torch = x1 + x2
 y_triton = add(x1, x2)
-print(y_torch)
-print(y_triton)
+print(f"torch={y_torch}")
+print(f"triton={y_triton}")
 print(
     f"The maximum difference between torch and triton is "
     f"{torch.max(torch.abs(y_torch - y_triton))}"
@@ -63,13 +63,13 @@ print(
 def benchmark(size, provider):
     x1 = torch.rand(size, device="cuda", dtype=torch.float32)
     x2 = torch.rand(size, device="cuda", dtype=torch.float32)
-    quantiles = [0.5, 0.2, 0.8]
+    qs = [0.5, 0.2, 0.8]
     if provider == "torch":
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: x1 + x2, quantiles=quantiles)
+        ms, min, max = triton.testing.do_bench(lambda: x1 + x2, quantiles=qs)
     if provider == "triton":
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: add(x1, x2), quantiles=quantiles)
-    gbps = lambda ms: 12 * size / ms * 1e-6
-    return gbps(ms), gbps(max_ms), gbps(min_ms)
+        ms, min, max = triton.testing.do_bench(lambda: add(x1, x2), quantiles=qs)
+    y = lambda ms: 12 * size / ms * 1e-6
+    return y(ms), y(max), y(min)
 
 
 # %%
