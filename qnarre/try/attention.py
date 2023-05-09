7aad59a19a629b60e73711314f9acaf7ec2d7243
quantapix <quantapix@gmail.com>
2023-05-09 20:08:20 +0000

    qpx

--- qnarre/try/attention.py
+++ qnarre/try/attention.py
@@ -42,9 +42,9 @@ def _fwd_kernel(
         l *= tl.exp(m - m2)
         p = tl.exp(qk - m2[:, None])
         l2 = tl.sum(p, 1) + l
-        l_rcp = 1. / l2
-        p *= l_rcp[:, None]
-        y *= (l * l_rcp)[:, None]
+        l3 = 1. / l2
+        p *= l3[:, None]
+        y *= (l * l3)[:, None]
         v = tl.load(vs + i * stride_vk)
         p = p.to(Q.dtype.element_ty)
         y += tl.dot(p, v)
@@ -57,24 +57,24 @@ def _fwd_kernel(
 
 @triton.jit
 def _bwd_preprocess(
-    Out, DO, L,
-    NewDO, Delta,
+    Y, DY, L,
+    NewDY, Delta,
     BLOCK_M: tl.constexpr, D_HEAD: tl.constexpr,
 ):
     off_m = tl.program_id(0) * BLOCK_M + tl.arange(0, BLOCK_M)
     off_n = tl.arange(0, D_HEAD)
-    o = tl.load(Out + off_m[:, None] * D_HEAD + off_n[None, :]).to(tl.float32)
-    do = tl.load(DO + off_m[:, None] * D_HEAD + off_n[None, :]).to(tl.float32)
+    y = tl.load(Y + off_m[:, None] * D_HEAD + off_n[None, :]).to(tl.float32)
+    dy = tl.load(DY + off_m[:, None] * D_HEAD + off_n[None, :]).to(tl.float32)
     denom = tl.load(L + off_m).to(tl.float32)
-    do = do / denom[:, None]
-    delta = tl.sum(o * do, axis=1)
-    tl.store(NewDO + off_m[:, None] * D_HEAD + off_n[None, :], do)
+    dy = dy / denom[:, None]
+    delta = tl.sum(y * dy, axis=1)
+    tl.store(NewDY + off_m[:, None] * D_HEAD + off_n[None, :], dy)
     tl.store(Delta + off_m, delta)
 
 
 @triton.jit
 def _bwd_kernel(
-    Q, K, V, sm_scale, Out, DO,
+    Q, K, V, sm_scale, Y, DY,
     DQ, DK, DV,
     L, M,
     D,
@@ -86,57 +86,54 @@ def _bwd_kernel(
     BLOCK_M: tl.constexpr, BLOCK_DMODEL: tl.constexpr,
     BLOCK_N: tl.constexpr,
 ):
-    off_hz = tl.program_id(0)
-    off_z = off_hz // H
-    off_h = off_hz % H
+    off = tl.program_id(0)
+    off_z = off // H
+    off_h = off % H
     Q += off_z * stride_qz + off_h * stride_qh
     K += off_z * stride_qz + off_h * stride_qh
     V += off_z * stride_qz + off_h * stride_qh
-    DO += off_z * stride_qz + off_h * stride_qh
     DQ += off_z * stride_qz + off_h * stride_qh
     DK += off_z * stride_qz + off_h * stride_qh
     DV += off_z * stride_qz + off_h * stride_qh
-    for start_n in range(0, num_block):
-        lo = start_n * BLOCK_M
+    DY += off_z * stride_qz + off_h * stride_qh
+    for i in range(0, num_block):
+        lo = i * BLOCK_M
         offs_qm = lo + tl.arange(0, BLOCK_M)
-        offs_n = start_n * BLOCK_M + tl.arange(0, BLOCK_M)
+        offs_n = i * BLOCK_M + tl.arange(0, BLOCK_M)
         offs_m = tl.arange(0, BLOCK_N)
         offs_k = tl.arange(0, BLOCK_DMODEL)
-        q_ptrs = Q + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        k_ptrs = K + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
-        v_ptrs = V + (offs_n[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        do_ptrs = DO + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        dq_ptrs = DQ + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        D_ptrs = D + off_hz * N_CTX
-        m_ptrs = M + off_hz * N_CTX
+        qs = Q + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        ks = K + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+        vs = V + (offs_n[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        dqs = DQ + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        dys = DY + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        ds = D + off * N_CTX
+        ms = M + off * N_CTX
         dv = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
         dk = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
-        k = tl.load(k_ptrs)
-        v = tl.load(v_ptrs)
-        for start_m in range(lo, num_block * BLOCK_M, BLOCK_M):
-            offs_m_curr = start_m + offs_m
-            q = tl.load(q_ptrs)
+        k = tl.load(ks)
+        v = tl.load(vs)
+        for j in range(lo, num_block * BLOCK_M, BLOCK_M):
+            j += offs_m
+            q = tl.load(qs)
             qk = tl.dot(q, tl.trans(k))
-            qk = tl.where(offs_m_curr[:, None] >= (offs_n[None, :]), qk, float("-inf"))
-            m = tl.load(m_ptrs + offs_m_curr)
+            qk = tl.where(j[:, None] >= (offs_n[None, :]), qk, float("-inf"))
+            m = tl.load(ms + j)
             p = tl.exp(qk * sm_scale - m[:, None])
-            do = tl.load(do_ptrs)
-            dv += tl.dot(tl.trans(p.to(Q.dtype.element_ty)), do)
-            Di = tl.load(D_ptrs + offs_m_curr)
-            dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - Di[:, None]
-            dp += tl.dot(do, tl.trans(v))
+            dy = tl.load(dys)
+            dv += tl.dot(tl.trans(p.to(Q.dtype.element_ty)), dy)
+            dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - tl.load(ds + j)[:, None]
+            dp += tl.dot(dy, tl.trans(v))
             ds = p * dp * sm_scale
             dk += tl.dot(tl.trans(ds.to(Q.dtype.element_ty)), q)
-            dq = tl.load(dq_ptrs)
+            dq = tl.load(dqs)
             dq += tl.dot(ds.to(Q.dtype.element_ty), k)
-            tl.store(dq_ptrs, dq)
-            dq_ptrs += BLOCK_M * stride_qm
-            q_ptrs += BLOCK_M * stride_qm
-            do_ptrs += BLOCK_M * stride_qm
-        dv_ptrs = DV + (offs_n[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        dk_ptrs = DK + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
-        tl.store(dv_ptrs, dv)
-        tl.store(dk_ptrs, dk)
+            tl.store(dqs, dq)
+            dqs += BLOCK_M * stride_qm
+            qs += BLOCK_M * stride_qm
+            dys += BLOCK_M * stride_qm
+        tl.store(DK + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk), dk)
+        tl.store(DV + (offs_n[:, None] * stride_qm + offs_k[None, :] * stride_qk), dv)
 
 
 empty = torch.empty(128, device="cuda")
--- qnarre/try/attention2.py
+++ qnarre/try/attention2.py
@@ -64,24 +64,24 @@ def _fwd_kernel(
 
 @triton.jit
 def _bwd_preprocess(
-    Out, DO, L,
-    NewDO, Delta,
+    Y, DY, L,
+    NewDY, Delta,
     BLOCK_M: tl.constexpr, D_HEAD: tl.constexpr,
 ):
     off_m = tl.program_id(0) * BLOCK_M + tl.arange(0, BLOCK_M)
     off_n = tl.arange(0, D_HEAD)
-    o = tl.load(Out + off_m[:, None] * D_HEAD + off_n[None, :]).to(tl.float32)
-    do = tl.load(DO + off_m[:, None] * D_HEAD + off_n[None, :]).to(tl.float32)
+    y = tl.load(Y + off_m[:, None] * D_HEAD + off_n[None, :]).to(tl.float32)
+    dy = tl.load(DY + off_m[:, None] * D_HEAD + off_n[None, :]).to(tl.float32)
     denom = tl.load(L + off_m).to(tl.float32)
-    do = do / denom[:, None]
-    delta = tl.sum(o * do, axis=1)
-    tl.store(NewDO + off_m[:, None] * D_HEAD + off_n[None, :], do)
+    dy = dy / denom[:, None]
+    delta = tl.sum(y * dy, axis=1)
+    tl.store(NewDY + off_m[:, None] * D_HEAD + off_n[None, :], dy)
     tl.store(Delta + off_m, delta)
 
 
 @triton.jit
 def _bwd_kernel(
-    Q, K, V, sm_scale, Out, DO,
+    Q, K, V, sm_scale, Y, DY,
     DQ, DK, DV,
     L, M,
     D,
@@ -93,57 +93,54 @@ def _bwd_kernel(
     BLOCK_M: tl.constexpr, BLOCK_DMODEL: tl.constexpr,
     BLOCK_N: tl.constexpr,
 ):
-    off_hz = tl.program_id(0)
-    off_z = off_hz // H
-    off_h = off_hz % H
+    off = tl.program_id(0)
+    off_z = off // H
+    off_h = off % H
     Q += off_z * stride_qz + off_h * stride_qh
     K += off_z * stride_qz + off_h * stride_qh
     V += off_z * stride_qz + off_h * stride_qh
-    DO += off_z * stride_qz + off_h * stride_qh
     DQ += off_z * stride_qz + off_h * stride_qh
     DK += off_z * stride_qz + off_h * stride_qh
     DV += off_z * stride_qz + off_h * stride_qh
-    for start_n in range(0, num_block):
-        lo = start_n * BLOCK_M
+    DY += off_z * stride_qz + off_h * stride_qh
+    for i in range(0, num_block):
+        lo = i * BLOCK_M
         offs_qm = lo + tl.arange(0, BLOCK_M)
-        offs_n = start_n * BLOCK_M + tl.arange(0, BLOCK_M)
+        offs_n = i * BLOCK_M + tl.arange(0, BLOCK_M)
         offs_m = tl.arange(0, BLOCK_N)
         offs_k = tl.arange(0, BLOCK_DMODEL)
-        q_ptrs = Q + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        k_ptrs = K + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
-        v_ptrs = V + (offs_n[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        do_ptrs = DO + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        dq_ptrs = DQ + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        D_ptrs = D + off_hz * N_CTX
-        m_ptrs = M + off_hz * N_CTX
+        qs = Q + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        ks = K + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+        vs = V + (offs_n[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        dqs = DQ + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        dys = DY + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+        ds = D + off * N_CTX
+        ms = M + off * N_CTX
         dv = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
         dk = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
-        k = tl.load(k_ptrs)
-        v = tl.load(v_ptrs)
-        for start_m in range(lo, num_block * BLOCK_M, BLOCK_M):
-            offs_m_curr = start_m + offs_m
-            q = tl.load(q_ptrs)
+        k = tl.load(ks)
+        v = tl.load(vs)
+        for j in range(lo, num_block * BLOCK_M, BLOCK_M):
+            j += offs_m
+            q = tl.load(qs)
             qk = tl.dot(q, k, trans_b=True)
-            qk = tl.where(offs_m_curr[:, None] >= (offs_n[None, :]), qk, float("-inf"))
-            m = tl.load(m_ptrs + offs_m_curr)
+            qk = tl.where(j[:, None] >= (offs_n[None, :]), qk, float("-inf"))
+            m = tl.load(ms + j)
             p = tl.exp(qk * sm_scale - m[:, None])
-            do = tl.load(do_ptrs)
-            dv += tl.dot(p.to(do.dtype), do, trans_a=True)
-            Di = tl.load(D_ptrs + offs_m_curr)
-            dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - Di[:, None]
-            dp += tl.dot(do, v, trans_b=True)
+            dy = tl.load(dys)
+            dv += tl.dot(p.to(dy.dtype), dy, trans_a=True)
+            dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - tl.load(ds + j)[:, None]
+            dp += tl.dot(dy, v, trans_b=True)
             ds = p * dp * sm_scale
             dk += tl.dot(ds.to(q.dtype), q, trans_a=True)
-            dq = tl.load(dq_ptrs, eviction_policy="evict_last")
+            dq = tl.load(dqs, eviction_policy="evict_last")
             dq += tl.dot(ds.to(k.dtype), k)
-            tl.store(dq_ptrs, dq, eviction_policy="evict_last")
-            dq_ptrs += BLOCK_M * stride_qm
-            q_ptrs += BLOCK_M * stride_qm
-            do_ptrs += BLOCK_M * stride_qm
-        dv_ptrs = DV + (offs_n[:, None] * stride_qm + offs_k[None, :] * stride_qk)
-        dk_ptrs = DK + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
-        tl.store(dv_ptrs, dv)
-        tl.store(dk_ptrs, dk)
+            tl.store(dqs, dq, eviction_policy="evict_last")
+            dqs += BLOCK_M * stride_qm
+            qs += BLOCK_M * stride_qm
+            dys += BLOCK_M * stride_qm
+        tl.store(DV + (offs_n[:, None] * stride_qm + offs_k[None, :] * stride_qk), dv)
+        tl.store(DK + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk), dk)
 
 
 class _attention(torch.autograd.Function):
