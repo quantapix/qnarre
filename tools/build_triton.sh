611a0a95317374e6fcb46249980bc01bb74edc37
quantapix <quantapix@gmail.com>
2023-05-14 20:57:34 +0000

    qpx

--- tools/build_triton.sh
+++ tools/build_triton.sh
@@ -20,9 +20,10 @@ ARGS="  \
         -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
         -DLLVM_ENABLE_ASSERTIONS=True \
         -DLLVM_ENABLE_WERROR=ON \
+        -DLLVM_EXTERNAL_LIT=$BUILD/llvm/bin/llvm-lit \
         -DLLVM_INCLUDE_DIRS=$BUILD/llvm/out/include \
         -DLLVM_LIBRARY_DIR=$BUILD/llvm/out/lib \
-        -DPYBIND11_INCLUDE_DIR=$BUILD/pybind11/out/include \
+        -DPYBIND11_INCLUDE_DIR=$BUILD/.env/lib64/python3.11/site-packages/pybind11/include \
         -DPYTHON_INCLUDE_DIRS=/usr/include/python3.11 \
         -DPython3_EXECUTABLE:FILEPATH=./.env/bin/python \
         -DTRITON_BUILD_PYTHON_MODULE=ON \
@@ -30,7 +31,6 @@ ARGS="  \
 "
 #        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
 #        -DCMAKE_BUILD_TYPE=Debug \
-#        -DLLVM_EXTERNAL_LIT= \
 
 # rm -rf "$BUILD"
 mkdir -p "$BUILD/triton"
@@ -38,7 +38,7 @@ pushd "$BUILD"
 if [ ! -e .env ]; then
     python3.11 -m venv .env
 fi
-.env/bin/pip install -U pip wheel setuptools
+.env/bin/pip install -U pip wheel setuptools pybind11
 popd
 pushd "$BUILD/triton"
 # cmake "$SOURCE" $ARGS
