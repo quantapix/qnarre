b205aecd103d8a436db32a07cc1f4e2bba8bd986
quantapix <quantapix@gmail.com>
2023-05-15 02:23:31 +0000

    qpx

--- tools/build.sh
+++ tools/build.sh
@@ -13,11 +13,17 @@ ARGS="  \
         -DCMAKE_MODULE_LINKER_FLAGS=-fuse-ld=lld \
         -DCMAKE_SHARED_LINKER_FLAGS=-fuse-ld=lld \
         -DLLVM_EXTERNAL_LIT=$BUILD/llvm/bin/llvm-lit \
+        -DLLVM_MINIMUM_PYTHON_VERSION=3.11 \
         -DMLIR_DIR=$BUILD/llvm/out/lib/cmake/mlir \
 "
 
 mkdir -p "$BUILD/standalone"
-pushd "$BUILD/standalone"
+pushd "$BUILD"
+VIRTUAL_ENV="$BUILD/.env"
+export VIRTUAL_ENV
+PATH="$BUILD/.env/bin:$PATH"
+export PATH
+cd standalone
 cmake -G Ninja -S "$SOURCE" $ARGS
 cmake --build . --target check-standalone
 popd
