1153c8139e87413f8bd0364ce2bd4510f7aa93a4
quantapix <quantapix@gmail.com>
2023-05-16 00:39:20 +0000

    qpx

--- tools/kaleidoscope/BuildingAJIT/toy.cpp
+++ tools/kaleidoscope/BuildingAJIT/toy.cpp
@@ -1,6 +1,4 @@
-#include "KaleidoscopeJIT.h"
 #include "llvm/ADT/APFloat.h"
-#include "llvm/ADT/STLExtras.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/DerivedTypes.h"
@@ -11,48 +9,17 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
-#include "llvm/Support/TargetSelect.h"
 #include "llvm/Target/TargetMachine.h"
 #include <algorithm>
 #include <cassert>
-#include <cctype>
 #include <cstdint>
-#include <cstdio>
 #include <cstdlib>
 #include <map>
 #include <memory>
-#include <string>
 #include <utility>
-#include <vector>
 
 using namespace llvm;
 using namespace llvm::orc;
-
-class PrototypeAST {
-  std::string Name;
-  std::vector<std::string> Args;
-  bool IsOperator;
-  unsigned Precedence; // Precedence if a binary op.
-
-public:
-  PrototypeAST(const std::string &Name, std::vector<std::string> Args,
-               bool IsOperator = false, unsigned Prec = 0)
-      : Name(Name), Args(std::move(Args)), IsOperator(IsOperator),
-        Precedence(Prec) {}
-
-  Function *codegen();
-  const std::string &getName() const { return Name; }
-
-  bool isUnaryOp() const { return IsOperator && Args.size() == 1; }
-  bool isBinaryOp() const { return IsOperator && Args.size() == 2; }
-
-  char getOperatorName() const {
-    assert(isUnaryOp() || isBinaryOp());
-    return Name[Name.size() - 1];
-  }
-
-  unsigned getBinaryPrecedence() const { return Precedence; }
-};
 
 static std::unique_ptr<ExprAST> ParseExpression();
 
