7a506f112e9463243f6348299e267bcd8247aaa8
quantapix <quantapix@gmail.com>
2023-05-19 14:02:53 +0000

    qpx

--- tools/toy/include/toy/AST.h
+++ tools/toy/include/toy/AST.h
@@ -1,17 +1,3 @@
-//===- AST.h - Node definition for the Toy AST ----------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-// This file implements the AST for the Toy language. It is optimized for
-// simplicity, not efficiency. The AST forms a tree structure where each node
-// references its children using std::unique_ptr<>.
-//
-//===----------------------------------------------------------------------===//
-
 #ifndef TOY_AST_H
 #define TOY_AST_H
 
@@ -20,9 +6,9 @@
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/Casting.h"
+#include <optional>
 #include <utility>
 #include <vector>
-#include <optional>
 
 namespace toy {
 
