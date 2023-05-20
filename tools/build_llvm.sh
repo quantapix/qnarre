833217a118fe3bf1ee46ed79e6770894a1dbb713
quantapix <quantapix@gmail.com>
2023-05-20 14:01:16 +0000

    qpx

--- tools/build_llvm.sh
+++ tools/build_llvm.sh
@@ -11,7 +11,7 @@ ARGS="  \
         -DCMAKE_BUILD_TYPE=MinSizeRel \
         -DCMAKE_C_COMPILER=clang \
         -DCMAKE_CXX_COMPILER=clang++ \
-        -DCMAKE_CXX_STANDARD=20 \
+        -DCMAKE_CXX_STANDARD=17 \
         -DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=lld \
         -DCMAKE_INSTALL_PREFIX=$BUILD/llvm/out \
         -DCMAKE_LINKER=lld \
@@ -22,6 +22,7 @@ ARGS="  \
         -DLLVM_INSTALL_UTILS=ON \
         -DLLVM_TARGETS_TO_BUILD=X86;NVPTX \
         -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
+        -DPython3_EXECUTABLE=$BUILD/.env/bin/python \
 "
 #        -DCMAKE_BUILD_TYPE=Release \
 #        -DCMAKE_BUILD_TYPE=Debug \
