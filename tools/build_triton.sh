14e501d1f842a5a8e680ff6e763f3f3141c660ac
quantapix <quantapix@gmail.com>
2023-05-21 16:36:52 +0000

    qpx

--- tools/build_triton.sh
+++ tools/build_triton.sh
@@ -11,8 +11,8 @@ ARGS="  \
         -DCMAKE_BUILD_TYPE=TritonRelBuildWithAsserts \
         -DCMAKE_C_COMPILER=clang \
         -DCMAKE_CXX_COMPILER=clang++ \
-        -DCMAKE_CXX_STANDARD=17 \
         -DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=lld \
+        -DCMAKE_INSTALL_PREFIX=$BUILD/triton/out \
         -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$BUILD/triton/out \
         -DCMAKE_LINKER=lld \
         -DCMAKE_MODULE_LINKER_FLAGS=-fuse-ld=lld \
@@ -23,8 +23,6 @@ ARGS="  \
         -DLLVM_LIBRARY_DIR=$BUILD/llvm/out/lib \
         -DPYBIND11_INCLUDE_DIR=$BUILD/.env/lib64/python3.11/site-packages/pybind11/include \
         -DPython3_EXECUTABLE=$BUILD/.env/bin/python \
-        -DTRITON_BUILD_PYTHON_MODULE=ON \
-        -DTRITON_BUILD_TUTORIALS=OFF \
 "
 #        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
 #        -DCMAKE_BUILD_TYPE=Debug \
