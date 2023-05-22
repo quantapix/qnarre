1f85c11d8775e0e221def63448bfa1530480a242
quantapix <quantapix@gmail.com>
2023-05-22 20:34:42 +0000

    qpx

---
+++ tools/kaleidoscope/lib.rs
@@ -0,0 +1,16 @@
+#![feature(box_syntax)]
+#![feature(convert)]
+#![feature(plugin)]
+#![plugin(regex_macros)]
+
+extern crate regex;
+
+extern crate iron_llvm;
+extern crate llvm_sys;
+
+pub mod builder;
+pub mod driver;
+pub mod filer;
+pub mod jitter;
+pub mod lexer;
+pub mod parser;
