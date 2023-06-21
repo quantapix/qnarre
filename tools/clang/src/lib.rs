39777bb418162b1bf1de30a66e379a4c4ae1fe4e
quantapix <quantapix@gmail.com>
2023-06-21 04:08:27 +0000

    qpx

--- tools/clang/src/lib.rs
+++ tools/clang/src/lib.rs
@@ -1,5 +1,8 @@
 #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals, dead_code)]
-//#![cfg_attr(feature = "cargo-clippy", allow(clippy::unreadable_literal))]
+#![cfg_attr(
+    feature = "cargo-clippy",
+    allow(clippy::unreadable_literal, clippy::missing_safety_doc, clippy::too_many_arguments)
+)]
 
 #[cfg(feature = "runtime")]
 extern crate libloading;
